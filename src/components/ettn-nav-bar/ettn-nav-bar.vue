d5f9931c74a2e936bc616e27f4f32216c5162a18
Shamil <you@example.com>
2019-09-29 22:17:24 +0600

    добавлено меню, многоязычность

---
+++ src/components/ettn-nav-bar/ettn-nav-bar.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ettn-nav-bar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/locale/se.js
@@ -0,0 +1,76 @@
+const se = {
+    common:{
+        language: { 
+            language: "Språk",
+            current: "Svenska",
+            se: "Svenska",
+            en: "English",
+            de: "Deutsch",
+            nl: "Nederlands"
+        },
+        languages: {
+            se: "Svenska",
+            en: "English",
+            de: "Deutsch",
+            nl: "Nederlands"
+        },
+        theme: {
+            theme: "Tema",
+            dark: "Mörkt",
+            light: "Ljust"
+        },
+        nav: {
+            location: "Adress",
+            about: "Om",
+            contact: "Kontakt",
+            contactSent: "Kontakt/skickat",
+            checkout: "Kassa",
+            checkoutCompleted: "Kassa/bekräftad",
+            welcome: "Välkommen",
+            products: "Produkter",
+            services: "Tjänster"
+        },
+        commands: {
+            submit: "Skicka",
+            cancel: "Avbryt",
+            next: "Nästa",
+            prev: "Föregående",
+            back: "Tillbaka"
+        },
+        form: {
+            first: "För",
+            firstName: "Förnamn",
+            last: "Efter",
+            lastName: "Efternamn",
+            name: "namn",
+            address: "Adress",
+            zipCode: "Postkod",
+            email: "Email",
+
+            inquiryId: "Förfrågan ID",
+            subject: "Ämnesrad",
+            message: "Meddelande",
+            
+            cart: "Varukorg",
+            checkout: "Kassa",
+            buy: "Köp",
+            moreInfo: "Mer info",
+            
+            sent: {
+                title: "Tack för din förfrågan!",
+                message: "Vi återkommer till er så snart som möjligt!"
+            },
+            
+            checkout: {
+                title: "Tack!",
+                message: "Alla ordrar behandlas manuellt, och ordern är inte bekräftad förrän ni får en bekräftelse av ansvarig personal.",
+                message2: "Vi återkommer till er så snart som möjligt!"
+            }
+        }
+    },
+    welcome: {
+        title: "Välkommen"
+    }
+}
+
+export default se
